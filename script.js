efea217eb67207d602c4c5dfad97f4238fe73f3b
Luksmed <lukksme@gmail.com>
2022-01-14 21:50:34 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,10 @@
+let name = "Lucas"
+let number = 10
+let conta = 2022 - number
+console.log(conta + number)
+
+console.log("Estou mexendo no console.log")
+
+let quartaFeira = true
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
